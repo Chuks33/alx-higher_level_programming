cacfaa8bb39fa2cd15d38c35eaa236c64865895e
Chuks33 <111119584+Chuks33@users.noreply.github.com>
2023-01-18 16:42:00 +0100

    Create 101-remove_char_at.py

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    new_str = ""
+    for i in range(0, len(str)):
+        if i != n:
+            new_str = new_str + str[i]
+        else:
+            continue
+    return new_str
