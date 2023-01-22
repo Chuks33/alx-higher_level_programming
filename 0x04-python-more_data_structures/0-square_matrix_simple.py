d5c85c745b3787d4ff1d4ba458f8d718a50b1c16
Chuks33 <chuksfrank375@gmail.com>
2023-01-21 18:16:58 -0800

    matrix square

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,7 @@
-#!/usr/bin/python3 
- def square_matrix_simple(matrix=[]): 
-     tmp = [] 
-     for x in matrix: 
-         tmp.append(list(map(lambda x: x**2, x))) 
-     return (tmp)
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_list = []
+    for i in range(0, len(matrix)):
+        new_list += [list(map(lambda x: x ** 2, matrix[i]))]
+    return new_list
+
