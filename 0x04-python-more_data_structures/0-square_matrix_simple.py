df367bd1bae01769aaff3f2b392f384f04bf43be
Chuks33 <111119584+Chuks33@users.noreply.github.com>
2023-01-22 02:48:12 +0100

    Delete 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3 
- def square_matrix_simple(matrix=[]): 
-     new_matrix = [] 
-     for row in (matrix): 
-             new_matrix.append([x*x for x in row]) 
-     return new_matrix
