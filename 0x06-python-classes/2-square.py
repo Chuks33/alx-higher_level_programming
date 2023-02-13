8b7847ad704fad98a600d3ba89a12313903abfb8
Chuks33 <111119584+Chuks33@users.noreply.github.com>
2023-02-13 13:50:11 +0100

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-""" Creates an empty class called Square
-"""
+
+"""Define a class Square."""
 
 
 class Square:
-    """ Empty class with size private attribute
-    """
+    """Represent a square."""
+
     def __init__(self, size=0):
-        """
-                Instantiation with size
+        """Initialize a new Square.
+        
         Args:
-            size: size of the square
+            size (int): The size of the new square.
         """
-        if type(size) is not int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = 
+        self.__size = size
