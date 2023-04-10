5655632837a909ebd607007124a95fc76bc7e733
Chuks33 <111119584+Chuks33@users.noreply.github.com>
2023-04-10 10:08:30 +0100

    Update 8-simple_delete.py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-def common_elements(set_1, set_2):
-    return list(set(set_1) & set(set_2))
+def simple_delete(a_dictionary, key=""):
+    if (key in a_dictionary):
+        del a_dictionary[key]
+    return 
