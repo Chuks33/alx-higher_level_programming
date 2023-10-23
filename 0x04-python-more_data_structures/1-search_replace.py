0d1128045d3d513c2f06f6592f91d7fbdaf85045
Chuks33 <111119584+Chuks33@users.noreply.github.com>
2023-10-23 18:09:35 +0100

    Delete 0x04-python-more_data_structures directory

--- 0x04-python-more_data_structures/1-search_replace.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-def search_replace(my_list, search, replace):
-    new_list = [i if i != search else replace for i in my_list]
-    return new_list
--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-def square_matrix_map(matrix=[]):
-    return list(map(lambda y: list(map(lambda x: x ** 2, y)), matrix))
