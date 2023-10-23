47b43b321db6f8df347a44b3be501557667901ec
Chuks33 <111119584+Chuks33@users.noreply.github.com>
2023-10-23 18:25:24 +0100

    Delete 0x02-python-import_modules directory

--- 0x02-python-import_modules/4-hidden_discovery.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-import hidden_4
-
-if __name__ == "__main__":
-    file = dir(hidden_4)
-    length = len(file)
-    for i in range(0, length):
-        if file[i][0:2] != "__":
-            print(file[i])
